8df5434f9db59f38f5c20ac0171a27b0f5f2bdcc
Saisii <sethconger@gmail.com>
2022-11-01 22:03:22 +0300

    solve problems

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,33 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    if (typeof first != 'number' || typeof last != 'number') {
+        console.log(typeof first);
+        console.log(typeof last);
+        return 'ERROR';
+    }
+    
+    else if (first < 0 || last < 0) {
+        console.log('2');
+        return 'ERROR';
+    }
+    
+    else if (first < last) {
+        let sum = first;
+        while (first < last) {
+            first++;
+            sum += first;
+        }
+        return sum;
+    }
+
+    else if (first > last) {
+        let sum = last;
+        while (last < first) {
+            last++;
+            sum += last;
+        }
+        return sum;
+    }
+
 
 };
 
