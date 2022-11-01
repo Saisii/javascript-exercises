bb6f0452602aa052e1655b62dc94074b339c3b10
Saisii <sethconger@gmail.com>
2022-11-01 23:00:16 +0300

    solve problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,63 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
+
+};
+
+const subtract = function(num1, num2) {
+	if (num1 >= num2) {
+    return num1 - num2;
+  }
+
+  else {
+    return num2 - num1;
+  }
+
+};
+
+const sum = function(arr) {
+  let numSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    numSum += arr[i];
+    // console.log(numSum);
+    
+  }
+	
+  return numSum; 
+};
+
+const multiply = function(numbers) {
+  let numMul = 1;
+  for (let i = 0; i < numbers.length; i++) {
+    numMul *= numbers[i];
+  }
+
+  return numMul;
+};
+
+const power = function(num, expon) {
+  let numExp = 1;
+  for (let i = 0; i < expon; i++) {
+    numExp *= num;
+  }
+
+  return numExp;
 	
 };
 
-const subtract = function() {
-	
-};
+const factorial = function(fact) {
+  let numFact = 1;
 
-const sum = function() {
-	
-};
+  if (fact === 0) {
+    return 1;
+  }
 
-const multiply = function() {
+  for (let i = 1; i <= fact; i++) {
+    
+    numFact *= i;
+  }
 
-};
+    return numFact;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
 	
 };
 
