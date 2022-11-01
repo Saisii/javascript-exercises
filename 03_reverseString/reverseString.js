e87adda35a55760e6dcc984d05b568cf3844d084
Saisii <sethconger@gmail.com>
2022-11-01 18:35:44 +0300

    solve problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    let reverse = '';
+    let i = stringArray.length-1;
+    while (i >= 0) {
+        reverse = reverse + stringArray[i];
+        i--;
+        console.log(reverse);
+    }
+
+    return reverse;
 
 };
 
