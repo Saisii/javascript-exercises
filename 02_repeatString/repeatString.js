6f425e723ab71944aa351d4dfbb5854f24254cc2
Saisii <sethconger@gmail.com>
2022-11-01 17:59:52 +0300

    complete task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let sentence = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        while (num >= 1) {
+            sentence = sentence + string;
+            num--;
+            // console.log(sentence);
+        }
+        return sentence;
+    }
 };
 
 // Do not edit below this line
