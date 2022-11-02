28a018456f9c9815bb89c7f3486dca7249253406
Saisii <sethconger@gmail.com>
2022-11-02 10:01:10 +0300

    solve problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    let fib = [1, 1];
+
+    for (i = 0; i <= num; i++) {
+        fib.push(fib[i] + fib[i + 1]);
+    }
+
+    console.log(fib);
+
+    return fib[num - 1];
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
