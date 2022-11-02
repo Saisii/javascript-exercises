9643b0dbd13c85de8a482201644cac78e55cca3d
Saisii <sethconger@gmail.com>
2022-11-02 10:20:14 +0300

    fix problem

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,17 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+
+const getTheTitles = function(books) {
+    return [books[0].title, books[1].title];
 
 };
 
